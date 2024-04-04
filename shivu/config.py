d030e47c2e1d1a2a10648a4c6959433cdc56b2cb
RealKronos69 <138235386+RealKronos69@users.noreply.github.com>
2024-04-04 08:54:03 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,19 +1,19 @@
 class Config(object):
     LOGGER = True
 
-    # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    # Get this value from my.telegram.org/a
+OWNER_ID = 5691178083
+sudo_users = "5691178083", "5791955148", "1938688666", "5930690809", "6831910507", "5151095145"
+GROUP_ID = "-1002137911091"
+TOKEN = "6906842854:AAGR61KidTvhia9MJvBCJVvt3yNPqhNAh60"
+mongo_url = "mongodb+srv://kronos:1q2w3e4r5t6y7u8i9o0p@kronos0.2hfeyzv.mongodb.net/?retryWrites=true&w=majority"
+PHOTO_URL = ["https://telegra.ph/file/11ef347fe9cca05e7ec8d.jpg", "https://telegra.ph/file/1551b798deb9a40508b2c.jpg", "https://telegra.ph/file/9c2fa21967a93d876deb9.jpg", "https://telegra.ph/file/900f73d2e740d7061b6f6.jpg", "https://telegra.ph/file/9b4fd107c50441bb80647.jpg"]
+SUPPORT_CHAT = "spartans_mainchats"
+UPDATE_CHAT = "waifuhunterlogz"
+BOT_USERNAME = "WaifuhunteRZbot"
+CHARA_CHANNEL_ID = "-1002011392805"
+api_id = 26626068
+api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
     
 class Production(Config):
